d09156096ff8c612b0d925933c233ac7b01d12f0
Corentin Leffy <corentin.leffy@gmail.com>
2020-02-29 13:35:48 +0100

    :recycle: Replace conditional with maps

--- kotlin/src/main/kotlin/com/adaptionsoft/games/uglytrivia/Game.kt
+++ kotlin/src/main/kotlin/com/adaptionsoft/games/uglytrivia/Game.kt
@@ -1,40 +1,55 @@
 package com.adaptionsoft.games.uglytrivia
 
 import java.util.ArrayList
+import java.util.Deque
+import java.util.EnumMap
 import java.util.LinkedList
 
+
 enum class Category {
-    Pop, Rock, Science, Sports
+    Pop, Science, Sports, Rock
 }
 
 class Game {
+    companion object {
+        private const val NB_CELLS = 12
+    }
+
+    private val categories: Array<Category>
+        get() = Category.values()
+    private val categoriesByPosition: MutableMap<Int, Category> = HashMap(NB_CELLS)
+    private val questionsByCategory: EnumMap<Category, Deque<String>> = EnumMap(Category::class.java)
+
     private var players = ArrayList<Any>()
     private var places = IntArray(6)
     private var purses = IntArray(6)
     private var inPenaltyBox = BooleanArray(6)
 
-    private var popQuestions = LinkedList<Any>()
-    private var scienceQuestions = LinkedList<Any>()
-    private var sportsQuestions = LinkedList<Any>()
-    private var rockQuestions = LinkedList<Any>()
+    private val currentPosition: Int
+        get() = places[currentPlayer]
+
+    private val currentCategory: Category
+        get() = categoriesByPosition[currentPosition]
+                ?: Category.Rock
 
     private var currentPlayer = 0
     private var isGettingOutOfPenaltyBox: Boolean = false
 
     init {
-        for (i in 0..49) {
-            popQuestions.addLast("Pop Question $i")
-            scienceQuestions.addLast("Science Question $i")
-            sportsQuestions.addLast("Sports Question $i")
-            rockQuestions.addLast(createRockQuestion(i))
+        for (category in categories) {
+            questionsByCategory[category] = LinkedList<String>()
+        }
+        for (i in 0 until 50) {
+            for (category in categories) {
+                questionsByCategory[category]?.addLast("$category Question $i")
+            }
+        }
+        for (i in 0 until NB_CELLS) {
+            categoriesByPosition[i] = categories[i % categories.size]
         }
     }
 
-    private fun createRockQuestion(index: Int): String = "Rock Question $index"
-
     fun add(playerName: String): Boolean {
-
-
         players.add(playerName)
         places[howManyPlayers()] = 0
         purses[howManyPlayers()] = 0
@@ -62,7 +77,7 @@ class Game {
                 println(players[currentPlayer].toString()
                         + "'s new location is "
                         + places[currentPlayer])
-                println("The category is " + currentCategory())
+                println("The category is $currentCategory")
                 askQuestion()
             } else {
                 println(players[currentPlayer].toString() + " is not getting out of the penalty box")
@@ -77,30 +92,15 @@ class Game {
             println(players[currentPlayer].toString()
                     + "'s new location is "
                     + places[currentPlayer])
-            println("The category is " + currentCategory())
+            println("The category is $currentCategory")
             askQuestion()
         }
 
     }
 
     private fun askQuestion() {
-        if (currentCategory() == Category.Pop)
-            println(popQuestions.removeFirst())
-        if (currentCategory() == Category.Science)
-            println(scienceQuestions.removeFirst())
-        if (currentCategory() == Category.Sports)
-            println(sportsQuestions.removeFirst())
-        if (currentCategory() == Category.Rock)
-            println(rockQuestions.removeFirst())
+        println(questionsByCategory[currentCategory]?.removeFirst())
     }
-
-    private fun currentCategory(): Category =
-            when (places[currentPlayer]) {
-                in arrayOf(0, 4, 8) -> Category.Pop
-                in arrayOf(1, 5, 9) -> Category.Science
-                in arrayOf(2, 6, 10) -> Category.Sports
-                else -> Category.Rock
-            }
 
     fun wasCorrectlyAnswered(): Boolean {
         if (inPenaltyBox[currentPlayer]) {
